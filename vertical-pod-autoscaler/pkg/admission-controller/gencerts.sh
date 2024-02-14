d23adc01fce08a9b695fc62f08866ee358118a1f
fidesdigitalinnovation <133220889+fidesdigitalinnovation@users.noreply.github.com>
2024-02-14 01:10:39 +0100

    Update gencerts.sh

--- vertical-pod-autoscaler/pkg/admission-controller/gencerts.sh
+++ vertical-pod-autoscaler/pkg/admission-controller/gencerts.sh
@@ -39,9 +39,9 @@ subjectAltName = DNS:vpa-webhook.kube-system.svc
 EOF
 
 # Create a certificate authority
-openssl genrsa -out ${TMP_DIR}/caKey.pem 2048
+openssl genrsa -out ${TMP_DIR}//caKey.pem 2048
 set +o errexit
-openssl req -x509 -new -nodes -key ${TMP_DIR}/caKey.pem -days 100000 -out ${TMP_DIR}/caCert.pem -subj "//CN=${CN_BASE}_ca" -addext "subjectAltName = DNS:${CN_BASE}_ca"
+openssl req -x509 -new -nodes -key ${TMP_DIR}//caKey.pem -days 100000 -out ${TMP_DIR}//caCert.pem -subj "//CN=${CN_BASE}_ca" -addext "subjectAltName = DNS:${CN_BASE}_ca"
 if [[ $? -ne 0 ]]; then
   echo "ERROR: Failed to create CA certificate for self-signing. If the error is \"unknown option -addext\", update your openssl version or deploy VPA from the vpa-release-0.8 branch."
   exit 1
@@ -49,13 +49,13 @@ fi
 set -o errexit
 
 # Create a server certificate
-openssl genrsa -out ${TMP_DIR}/serverKey.pem 2048
+openssl genrsa -out ${TMP_DIR}//serverKey.pem 2048
 # Note the CN is the DNS name of the service of the webhook.
-openssl req -new -key ${TMP_DIR}/serverKey.pem -out ${TMP_DIR}/server.csr -subj "/CN=vpa-webhook.kube-system.svc" -config ${TMP_DIR}/server.conf
-openssl x509 -req -in ${TMP_DIR}/server.csr -CA ${TMP_DIR}/caCert.pem -CAkey ${TMP_DIR}/caKey.pem -CAcreateserial -out ${TMP_DIR}/serverCert.pem -days 100000 -extensions SAN -extensions v3_req -extfile ${TMP_DIR}/server.conf
+openssl req -new -key ${TMP_DIR}//serverKey.pem -out ${TMP_DIR}//server.csr -subj "//CN=vpa-webhook.kube-system.svc" -config ${TMP_DIR}//server.conf
+openssl x509 -req -in ${TMP_DIR}//server.csr -CA ${TMP_DIR}//caCert.pem -CAkey ${TMP_DIR}//caKey.pem -CAcreateserial -out ${TMP_DIR}//serverCert.pem -days 100000 -extensions SAN -extensions v3_req -extfile ${TMP_DIR}//server.conf
 
 echo "Uploading certs to the cluster."
-kubectl create secret --namespace=kube-system generic vpa-tls-certs --from-file=${TMP_DIR}/caKey.pem --from-file=${TMP_DIR}/caCert.pem --from-file=${TMP_DIR}/serverKey.pem --from-file=${TMP_DIR}/serverCert.pem
+kubectl create secret --namespace=kube-system generic vpa-tls-certs --from-file=${TMP_DIR}//caKey.pem --from-file=${TMP_DIR}//caCert.pem --from-file=${TMP_DIR}//serverKey.pem --from-file=${TMP_DIR}//serverCert.pem
 
 # Clean up after we're done.
 echo "Deleting ${TMP_DIR}."
